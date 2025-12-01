9bad23e1e4d66ac22ba6927b3f884210832dc088
ayxan hasanzada <@ayxan.hv>
2025-12-01 11:36:02 +0400

    add test2

--- src/components/SimpleAd.jsx
+++ src/components/SimpleAd.jsx
@@ -2,7 +2,13 @@ import { useEffect } from "react";
 
 export default function SimpleAd() {
   useEffect(() => {
+    const container = document.getElementById("ad-qvb-bottom");
+    if (!container) return;
+
+    container.innerHTML = "";
+
     const script1 = document.createElement("script");
+    script1.type = "text/javascript";
     script1.innerHTML = `
       atOptions = {
         'key' : '20d70c6c0ce17b616bdc5d617f96a591',
@@ -14,10 +20,9 @@ export default function SimpleAd() {
     `;
 
     const script2 = document.createElement("script");
+    script2.type = "text/javascript";
     script2.src = "//www.highperformanceformat.com/20d70c6c0ce17b616bdc5d617f96a591/invoke.js";
 
-    const container = document.getElementById("ad-qvb-bottom");
-    container.innerHTML = "";
     container.appendChild(script1);
     container.appendChild(script2);
   }, []);
